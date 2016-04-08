1b417b039dd77e2ed077d26a14468eb2e271d047
Sean Z.X. Pan <zpan@spin.CS.Princeton.EDU>
2016-04-08 12:29:17 -0400

    diffusion works

--- sumprobability.m
+++ sumprobability.m
@@ -1,5 +1,5 @@
 allprob = zeros(10000,3);
-
+tic
 for i = 1:10000
     testx = toTest(i,1);
     testy = toTest(i,2);
@@ -9,12 +9,19 @@ for i = 1:10000
         trainy = txcopy(j,2);
         trainv = txcopy(j,3);
         sep = [testx-trainx testy-trainy];
-        mu = [trainx trainy];
-        sigma = [trainv 0; 0 trainv];
+        value = 0;
+        if norm(sep) < 1000
+            mu = [trainx trainy];
+            sigma = [trainv 0; 0 trainv];
         
-        mvn = @(x,y) 1./(2 .* pi .* trainv .^2) .* exp(-1./2 .* ((x-trainx) .^ 2 ./ (trainv .^2) + (y-trainy) .^ 2 ./ (trainv .^2)));
-        value = integral2(mvn, testx-0.5,testx+0.5,testy-0.5,testy+0.5);
+            mvn = @(x,y) 1./(2 .* pi .* trainv .^2) .* exp(-1./2 .* ((x-trainx) .^ 2 ./ (trainv .^2) + (y-trainy) .^ 2 ./ (trainv .^2)));
+            value = integral2(mvn, testx-0.5,testx+0.5,testy-0.5,testy+0.5);
+        end
         prob = prob + value;
+    end
+    if mod(i,10) == 0
+        disp(i)
+        toc
     end
     allprob(i,:) = [testx testy prob];
 end
