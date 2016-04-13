fcabc95e23285ab9c0ead877f4f9f49ce0d62680
Sean Z.X. Pan <zpan@spin.CS.Princeton.EDU>
2016-04-12 20:40:14 -0400

    sklearn nmf working

--- nmftrial.py
+++ nmftrial.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import csv
 from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import NMF
 from scipy.sparse import csr_matrix
@@ -14,5 +15,34 @@ dftrain = pd.read_csv('txTripletsCounts.txt',
 dftrain['transaction'].describe()
 
 
+dftest = pd.read_csv('data/testTriplets.txt',
+                     header=None, 
+                     index_col=None, 
+                     sep=' ', 
+                     names=['sender','receiver','transaction'])
+
+train_csr = csr_matrix((dftrain['transaction'],(dftrain['sender'],dftrain['receiver'])),
+                                       shape=(444075,444075), dtype=float)
+
+test_csr = csr_matrix((dftest['transaction'],(dftest['sender'],dftest['receiver'])),
+                                       shape=(444075,444075), dtype=float)
+
+
+
+
+
 model = NMF(n_components=10, init='random', random_state=0, tol=0.01)
-trans = model.fit_transform(csr_matrix(dftrain)) 
+W = model.fit_transform(csr_matrix(train_csr))
+H = model.components_
+
+print W.shape
+print H.shape
+pred = []
+
+for index, row in dftest.iterrows():
+        pred.append(np.dot(W[row['sender'],:],H[:,row['receiver']]))
+
+
+with open('pred','w') as f1:
+        wrtest = csv.writer(f1, quoting=csv.QUOTE_NONNUMERIC,lineterminator='\n')
+        wrtest.writerow(pred)
--- snmf.py
+++ snmf.py
@@ -1,18 +1,11 @@
 
-# coding: utf-8
-
-# In[ ]:
-
-get_ipython().magic(u'matplotlib inline')
-get_ipython().magic(u'pylab inline')
-pylab.rcParams['figure.figsize'] = (20, 5)
-
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
 from sklearn.metrics import roc_curve, auc
+import nimfa
 
 dftrain = pd.read_csv('txTripletsCounts.txt',
                       header=None,
@@ -21,15 +14,12 @@ dftrain = pd.read_csv('txTripletsCounts.txt',
                       names=['sender','receiver','transaction'])
 dftrain['transaction'].describe()
 
-import nimfa
-
-#W=csr_matrix()
-
 snmf = nimfa.Snmf(csr_matrix(dftrain),seed="random_c", rank=10, max_iter=12, version='r', eta=1.,
                   beta=1e-4, i_conv=10, w_min_change=0)
 
-snmf.factorize()
+snmf_fit = snmf()
 
 W=snmf.basis();
 H=snmf.coef();
 
+print W[0]
